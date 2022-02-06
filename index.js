f03452ee59ef6d63404f88f419bd9c65eba3c690
Airgrinder <51jework@mail.com>
2022-02-06 18:23:56 +0300

    feat: create html, css, and js files, write project

---
+++ index.js
@@ -0,0 +1,37 @@
+const switchButtons = document.querySelectorAll('.header-nav-item')
+const playBtn = document.querySelector('.play')
+const main = document.querySelector('main')
+
+const audio = new Audio()
+
+
+let isPlay = false
+audio.src = 'assets/audio/forest.mp3'
+
+
+function play() {
+  audio.currentTime = 0
+  if (isPlay) {   //if true => stop
+    playBtn.style.backgroundImage = 'url(assets/svg/play.svg)'
+    audio.pause()
+    return isPlay = false
+  }
+  playBtn.style.backgroundImage = 'url(assets/svg/pause.svg)' // else play
+  console.log('test')
+  audio.play()
+  isPlay = true
+}
+
+function switcher(s) {
+  audio.src = 'assets/audio/' + s + '.mp3'
+  main.style.backgroundImage = 'url(assets/img/' + s + '.jpg)'
+  if (isPlay) {
+    audio.play()
+  }
+}
+
+for (const button of switchButtons) {
+  button.addEventListener('click', () => switcher(button.classList[1]))
+}
+
+playBtn.addEventListener('click', play)
